cf498ff5c88594cd1c53be33bcfa06977e759515
Sebastian Kasanzew <sebkasanzew@gmail.com>
2024-10-27 21:23:27 +0100

    bug: fix snapshot test

--- tests/specs/main.spec.ts
+++ tests/specs/main.spec.ts
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test'
 test('initial', async ({ page }) => {
 	await page.goto('./')
 
-	await expect(page).toHaveScreenshot({ maxDiffPixels: 30 })
+	await expect(page.getByText('rotation')).toBeVisible()
+	await expect(page).toHaveScreenshot({ maxDiffPixels: 50 })
 })
